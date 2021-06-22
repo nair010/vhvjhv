05fd0529766b5c2afaa52739f926fd958b52194a
nair010 <86296930+nair010@users.noreply.github.com>
2021-06-22 17:18:57 +0530

    Update abt.js

--- abt.js
+++ abt.js
@@ -1,3 +1,25 @@
 var scene = document.getElementById("scene");
 var parallaxInstance = new Parallax(scene);
 
+var keys = [
+  "ABOUT ROTARCT CLUB",
+  "ABOUT RCPU",
+  "OUR MISSION",
+  "OUR VISION",
+  
+];
+var slider = new Swiper(".swiper-container", {
+  // Optional parameters
+  slidesPerView: "auto",
+  spaceBetween: 150,
+  centeredSlides: true,
+  mousewheel: true,
+  pagination: {
+    el: ".planet-links",
+    clickable: true,
+    renderBullet: function(index, className) {
+      return '<div class="' + className + '">' + keys[index] + "</div>";
+    }
+  }
+});
+
